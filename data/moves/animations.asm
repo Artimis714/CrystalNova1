5fa586312085b5aefaadc58dc752cfac9969f019
Artimis714 <archaeoastronomer63@gmail.com>
2025-03-31 14:07:12 +0100

    New Hail Animation
    Created a new battle animation for Hail based on the tutorial. It required some modification as the constants have changed in Pokecrystal since that tutorial was written.

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -3462,6 +3462,20 @@ BattleAnim_Spite:
 
 BattleAnim_Hail:
 BattleAnim_InHail:
+	anim_1gfx BATTLE_ANIM_GFX_ICE
+	anim_bgeffect BATTLE_BG_EFFECT_WHITE_HUES, $0, $8, $0
+	anim_obj BATTLE_ANIM_OBJ_HAIL, 88, 0, $0
+	anim_wait 8
+	anim_obj BATTLE_ANIM_OBJ_HAIL, 72, 0, $1
+	anim_wait 8
+	anim_obj BATTLE_ANIM_OBJ_HAIL, 56, 0, $2
+.loop
+	anim_sound 0, 1, SFX_SHINE
+	anim_wait 8
+	anim_loop 8, .loop
+	anim_wait 8
+	anim_ret
+
 BattleAnim_PowderSnow:
 	anim_1gfx BATTLE_ANIM_GFX_ICE
 .loop
